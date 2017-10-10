f79211ee1467e570171d6395e0a3f542e6bb4e03
Nicolas Setton <setton@adacore.com>
2017-10-10 11:50:22 -0400

    Allow other hosts to connect to the server
    This allows a machine to serve contents for other servers.

--- compile_server/settings.py
+++ compile_server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ci!nks6jg!qxsk2gc1hpdld=&kxqqqotm)(5$^ork&=o#t3+v$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
