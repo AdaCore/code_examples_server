1dc8f138841d34221567cee7eb811843706929b2
Robert Tice <tice@adacore.com>
2018-04-12 14:17:34 -0400

    Adding a simple single procedure overrideable example template.

---
+++ resources/templates/simple_main/simple_main.adb
@@ -0,0 +1,14 @@
+--  #region withs
+--  #endregion withs
+--  begin readonly
+procedure Main is
+--  end readonly
+--  #region declare
+--  #endregion declare
+--  begin readonly
+begin
+--  end readonly
+--  #region body
+--  #endregion body
+--  begin readonly
+end Main;
