7454231b021a5cfa878fc8b8dfb7900785967f3e
Robert Tice <tice@adacore.com>
2018-04-12 15:10:36 -0400

    Adding blank lines in template.

--- resources/templates/simple_main/simple_main.adb
+++ resources/templates/simple_main/simple_main.adb
@@ -1,14 +1,17 @@
 --  #region withs
+
 --  #endregion withs
 --  begin readonly
 procedure Main is
 --  end readonly
 --  #region declare
+
 --  #endregion declare
 --  begin readonly
 begin
 --  end readonly
 --  #region body
+
 --  #endregion body
 --  begin readonly
 end Main;
