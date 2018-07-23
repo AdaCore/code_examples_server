6a24907d841beb4417d278e19e721f92d0cb3650
Nicolas Setton <setton@adacore.com>
2018-07-23 01:26:17 -0400

    Add a step to cleanup the sessions on the container

--- infrastructure/cleanup_sessions.sh
+++ infrastructure/cleanup_sessions.sh
@@ -3,8 +3,9 @@
 # Kill all old processes
 lxc exec safecontainer -- killall  -u unprivileged --older-than 30s -signal KILL
 
-# Delete all old directories
+# Delete all old directories, both in container and locally
 lxc exec safecontainer -- find /tmp/ -mindepth 1  -type d -mmin +1 -exec rm -rf {}  \;
+find /tmp/ -mindepth 1  -type d -mmin +1 -exec rm -rf {}  \;
 
 (if [ -d /webapps/compile_server/bin/ ] ; then
     export PATH=/webapps/compile_server/bin/:$PATH
