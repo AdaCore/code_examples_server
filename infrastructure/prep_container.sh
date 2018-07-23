afd8a8e8244ad02e65b249cba6f8be9e8e561f9a
Nicolas Setton <setton@adacore.com>
2018-07-23 01:25:49 -0400

    Protect a bit /var/tmp on the container

--- infrastructure/prep_container.sh
+++ infrastructure/prep_container.sh
@@ -2,6 +2,7 @@
 
 # Prevent unprivilegeds from writing to /tmp
 lxc exec safecontainer -- chmod 755 /tmp/
+lxc exec safecontainer -- chmod 755 /var/tmp/
 
 # Prevent the container from having internet access
 lxc exec safecontainer -- ifconfig eth0 down
